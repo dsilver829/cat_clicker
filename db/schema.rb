41da2c42705e94b7314343301334ede65439d5ac
David Silver <dsilver@candidatemetrics.com>
2015-12-23 15:54:28 -0800

    supporting multiple cats!

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223172641) do
+ActiveRecord::Schema.define(version: 20151223234855) do
 
   create_table "cats", force: :cascade do |t|
     t.integer  "clicks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+    t.string   "name"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-cat = Cat.create(clicks: 0)
+cat = Cat.create(name: 'Cat', clicks: 0)
 File.open(Rails.root + 'public' + 'cat.jpg') do |f|
   cat.image = f
 end
 cat.save!
+
+chewie = Cat.create(name: 'Chewie', clicks: 0)
+File.open(Rails.root + 'public' + 'chewie.jpg') do |f|
+  chewie.image = f
+end
+chewie.save!
